1ce2a2e6b2d1cf115f6466870c7bffab6ae2fd80
Alberto Ruiz - 20012696 <aarp@galileo.edu>
2011-05-08 17:55:19 -0500

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProyRestM::Application.config.secret_token = 'd08f027f2aedbe0e41b3c2a1090ebf353c0f8b630d50a1d107c8f3deb3028f57eef59cd640b6c93083776ef81f668dfd392ac8d65558c43c2083f4943d6d826b'
